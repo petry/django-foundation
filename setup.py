0ef01c075b8ea607e40a89ea99d3a2a7b58c8927
Marcos Daniel Petry <marcospetry@gmail.com>
2013-01-07 21:48:13 -0200

    added license

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author="Marcos Daniel Petry",
     author_email="marcospetry@gmail.com",
     url="https://github.com/petry/django-foundation",
+    license='Apache License 2.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
